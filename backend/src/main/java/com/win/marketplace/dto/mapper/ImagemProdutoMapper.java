87a7d38ada01758a5ae3f912165cd51b69f622a7
Agenor <agenoralencaar@gmail.com>
2026-01-03 10:24:06 -0300

    fix: Adicionar método de conversão OffsetDateTime para LocalDateTime no ImagemProdutoMapper

--- backend/src/main/java/com/win/marketplace/dto/mapper/ImagemProdutoMapper.java
+++ backend/src/main/java/com/win/marketplace/dto/mapper/ImagemProdutoMapper.java
@@ -3,8 +3,9 @@ package com.win.marketplace.dto.mapper;
 import com.win.marketplace.dto.response.ImagemProdutoResponseDTO;
 import com.win.marketplace.model.ImagemProduto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
@@ -13,4 +14,8 @@ public interface ImagemProdutoMapper {
     ImagemProdutoResponseDTO toResponseDTO(ImagemProduto imagemProduto);
 
     List<ImagemProdutoResponseDTO> toResponseDTOList(List<ImagemProduto> imagensProduto);
+    
+    default LocalDateTime map(OffsetDateTime offsetDateTime) {
+        return offsetDateTime != null ? offsetDateTime.toLocalDateTime() : null;
+    }
 }
